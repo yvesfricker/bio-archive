4bdaa6981a5cb03add04869d291089ecba920eeb
Yves Fricker <yvesfricker@icloud.com>
2024-12-11 13:03:15 +0100

    Correct footer styling
    Form logic not implemented

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,15 +4,17 @@
 			<div data-w-id="c38e0493-3a23-40a3-cdff-747a961f29c4" class="footer_component">
 				<div class="footer_link-block">
 					<div class="footer_item is-first">
-						<a href="our-food.html" class="footer_link">Unser Futter</a>
-						<a href="how-it-works.html" class="footer_link">So funktioniert&#x27;s</a>
-						<a href="our-story.html" class="footer_link">Über uns</a>
+						<a href="https://www.bellanatura.bio/our-food" class="footer_link">Unser Futter</a>
+						<a href="https://www.bellanatura.bio/how-it-works" class="footer_link"
+							>So funktioniert&#x27;s</a
+						>
+						<a href="https://www.bellanatura.bio/our-story" class="footer_link">Über uns</a>
 					</div>
 					<div class="footer_item is-second">
 						<a href="#" class="footer_link">AGB</a>
 						<a href="#" class="footer_link">Datenschutz</a>
-						<a href="#" class="footer_link">Cookie-Einstellungen</a>
-						<a href="mailto:bellanatura@gmail.com" class="footer_link">kontakt@bellanatura.bio</a>
+						<a href="mailto:kontakt@bellanatura.bio" class="footer_link">kontakt@bellanatura.bio</a>
+						<a href="mailto:bellanatura" class="footer_link">@bellanatura.bio</a>
 					</div>
 				</div>
 				<div class="footer_item is-last">
@@ -21,14 +23,7 @@
 						Abonniere unseren Newsletter für aktuelle Neuigkeiten &amp; tolle Angebote.
 					</p>
 					<div class="footer_form w-form">
-						<form
-							id="wf-form-Email-Form-Footer"
-							name="wf-form-Email-Form-Footer"
-							data-name="Email Form Footer"
-							method="get"
-							data-wf-page-id="64199ef9753feb3348421caa"
-							data-wf-element-id="082c4f4f-3498-7542-9424-5c0ab70c5aa6"
-						>
+						<form>
 							<input
 								class="footer_form-field w-input"
 								maxlength="256"
@@ -37,6 +32,7 @@
 								placeholder="Email-Adresse"
 								type="email"
 								id="Email-Form-Footer"
+								required=""
 							/><label class="w-checkbox footer_checkbox">
 								<div class="w-checkbox-input w-checkbox-input--inputType-custom checkbox"></div>
 								<input
@@ -55,35 +51,25 @@
 								value="Anmelden"
 							/>
 						</form>
-						<!-- <div class="w-form-done">
-							<div>Thank you! Your submission has been received!</div>
-						</div>
-						<div class="w-form-fail">
-							<div>Oops! Something went wrong while submitting the form.</div>
-						</div> -->
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- <img
-		src="/images/Bella-Natura_Logo_1_White.svg"
+	<img
+		src="images/Bella-Natura_Logo_1_White.svg"
 		loading="eager"
 		alt=""
 		class="footer_logo-image"
 	/><img
-		src="/images/bot-left-green.svg"
+		src="images/bot-left-green.svg"
 		loading="eager"
 		alt=""
 		class="img-flower_bottom-left"
-	/><img src="/images/bot-right-green.svg" loading="eager" alt="" class="img-flower_bottom-right" /> -->
+	/><img src="images/bot-right-green.svg" loading="eager" alt="" class="img-flower_bottom-right" />
 	<div class="footer_absolute-bg"></div>
 </footer>
 
 <style module>
 	@import '../../bella-natura.webflow.css';
-
-  footer * {
-    font-family: 'Nunito';
-  }
 </style>
